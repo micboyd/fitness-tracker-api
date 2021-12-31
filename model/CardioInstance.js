a13d6fa9ea28f8c6eed8eeccbb057c7a6cc1a0aa
Michael Boyd <michael_boyd@live.co.uk>
2021-12-31 15:24:52 +0000

    field update

--- model/CardioInstance.js
+++ model/CardioInstance.js
@@ -7,6 +7,9 @@ const cardioInstanceSchema = new mongoose.Schema({
     cardioName: {
         type: String,
     },
+    cardioType: {
+        type: String,
+    },
     date: {
         type: Date,
         default: Date.now
